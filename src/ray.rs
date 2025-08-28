ade3e715f015b7445385b9164b8e2bdbb36f2eac
Raforawesome <ubuntuhasnoroblox@gmail.com>
2025-08-28 13:18:02 -0400

    Create ray class

---
+++ src/ray.rs
@@ -0,0 +1,24 @@
+use crate::{Point3, Vec3};
+
+pub struct Ray {
+    origin: Point3,
+    dir: Vec3,
+}
+
+impl Ray {
+    pub fn new(origin: Point3, dir: Vec3) -> Self {
+        Ray { origin, dir }
+    }
+
+    pub fn at(&self, t: f64) -> Point3 {
+        self.origin + self.dir * t
+    }
+
+    pub const fn origin(&self) -> Point3 {
+        self.origin
+    }
+
+    pub const fn dir(&self) -> Vec3 {
+        self.dir
+    }
+}
