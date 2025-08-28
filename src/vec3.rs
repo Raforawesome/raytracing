99aae8692e601c7adf7c9703fe04eeb195e800f9
Raforawesome <ubuntuhasnoroblox@gmail.com>
2025-08-28 13:17:28 -0400

    Create Vec3 and all its methods

---
+++ src/vec3.rs
@@ -0,0 +1,123 @@
+use std::ops::{AddAssign, Div, DivAssign, Index, IndexMut, Mul, MulAssign, Neg};
+
+#[derive(Debug, Copy, Clone)]
+pub struct Vec3(pub f64, pub f64, pub f64);
+
+impl Vec3 {
+    pub const fn zero() -> Self {
+        Vec3(0.0, 0.0, 0.0)
+    }
+
+    pub fn new(x: f64, y: f64, z: f64) -> Self {
+        Vec3(x, y, z)
+    }
+
+    pub fn length_squared(&self) -> f64 {
+        self.0 * self.0 + self.1 * self.1 + self.2 * self.2
+    }
+
+    pub fn length(&self) -> f64 {
+        self.length_squared().sqrt()
+    }
+
+    pub fn dot(&self, other: &Self) -> f64 {
+        self.0 * other.0 + self.1 * other.1 + self.2 * other.2
+    }
+
+    pub fn cross(&self, other: &Self) -> Self {
+        Vec3(
+            self.1 * other.2 - self.2 * other.1,
+            self.2 * other.0 - self.0 * other.2,
+            self.0 * other.1 - self.1 * other.0,
+        )
+    }
+
+    pub fn unit(&self) -> Self {
+        let len: f64 = self.length();
+        *self / len
+    }
+
+    pub const fn x(&self) -> f64 {
+        self.0
+    }
+
+    pub const fn y(&self) -> f64 {
+        self.1
+    }
+
+    pub const fn z(&self) -> f64 {
+        self.2
+    }
+}
+
+impl Neg for Vec3 {
+    type Output = Self;
+
+    fn neg(self) -> Self::Output {
+        Vec3(-self.0, -self.1, -self.2)
+    }
+}
+
+impl Index<usize> for Vec3 {
+    type Output = f64;
+
+    fn index(&self, index: usize) -> &Self::Output {
+        match index {
+            0 => &self.0,
+            1 => &self.1,
+            2 => &self.2,
+            _ => panic!("Invalid index {} for Vec3 (must be < 3)", index),
+        }
+    }
+}
+
+impl IndexMut<usize> for Vec3 {
+    fn index_mut(&mut self, index: usize) -> &mut f64 {
+        match index {
+            0 => &mut self.0,
+            1 => &mut self.1,
+            2 => &mut self.2,
+            _ => panic!("Invalid index {} for Vec3 (must be < 3)", index),
+        }
+    }
+}
+
+impl AddAssign for Vec3 {
+    fn add_assign(&mut self, rhs: Self) {
+        self.0 += rhs.0;
+        self.1 += rhs.1;
+        self.2 += rhs.2;
+    }
+}
+
+impl MulAssign<f64> for Vec3 {
+    fn mul_assign(&mut self, rhs: f64) {
+        self.0 *= rhs;
+        self.1 *= rhs;
+        self.2 *= rhs;
+    }
+}
+
+impl Mul<f64> for Vec3 {
+    type Output = Self;
+
+    fn mul(self, rhs: f64) -> Self::Output {
+        Vec3(self.0 * rhs, self.1 * rhs, self.2 * rhs)
+    }
+}
+
+impl DivAssign<f64> for Vec3 {
+    fn div_assign(&mut self, rhs: f64) {
+        let recip: f64 = 1.0 / rhs;
+        *self *= recip; // multiplying by reciprocal is faster than 3 div ops
+    }
+}
+
+impl Div<f64> for Vec3 {
+    type Output = Self;
+
+    fn div(self, rhs: f64) -> Self::Output {
+        let recip: f64 = 1.0 / rhs;
+        self * recip // multiplying by reciprocal is faster than 3 div ops
+    }
+}
