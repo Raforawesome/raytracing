3182eab0adebf1089bedcae5a99491dc90d2dfb8
Raforawesome <ubuntuhasnoroblox@gmail.com>
2025-08-29 14:15:51 -0400

    Create `Hittable` trait and `HitRecord`

---
+++ src/hittable.rs
@@ -0,0 +1,11 @@
+use crate::{Point3, Vec3, ray::Ray};
+
+pub struct HitRecord {
+    pub point: Point3,
+    pub normal: Vec3,
+    pub t: f64,
+}
+
+pub trait Hittable {
+    fn hit(&self, ray: &Ray, ray_tmin: f64, ray_tmax: f64) -> Option<HitRecord>;
+}
