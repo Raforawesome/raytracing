fc81aaf154c199b97696db50906c31d46b1e880d
Raforawesome <ubuntuhasnoroblox@gmail.com>
2025-08-29 08:53:59 -0400

    Implement `Sub<Vec3>` for `Point3`

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@ pub mod color;
 pub mod ray;
 pub mod vec3;
 
-use std::ops::Add;
+use std::ops::{Add, Sub};
 
 pub use vec3::Vec3;
 
@@ -26,6 +26,18 @@ impl Add<Vec3> for Point3 {
             self.0.0 + other.0,
             self.0.1 + other.1,
             self.0.2 + other.2,
+        ))
+    }
+}
+
+impl Sub<Vec3> for Point3 {
+    type Output = Self;
+
+    fn sub(self, other: Vec3) -> Self::Output {
+        Point3(Vec3(
+            self.0.0 - other.0,
+            self.0.1 - other.1,
+            self.0.2 - other.2,
         ))
     }
 }
