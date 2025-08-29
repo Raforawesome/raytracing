05abd97ded333aab726b295038766b56207ba0ac
Raforawesome <ubuntuhasnoroblox@gmail.com>
2025-08-29 12:58:54 -0400

    Sphere hitscan logic

--- src/ray.rs
+++ src/ray.rs
@@ -22,3 +22,19 @@ impl Ray {
         self.dir
     }
 }
+
+impl Ray {
+    pub fn hit_sphere(&self, center: Point3, radius: f64) -> f64 {
+        let oc: Vec3 = center - self.origin();
+        let a: f64 = self.dir().dot(&self.dir());
+        let b: f64 = -2.0 * oc.dot(&self.dir());
+        let c: f64 = oc.dot(&oc) - radius * radius;
+        let discrim: f64 = b * b - 4.0 * a * c;
+
+        if discrim < 0.0 {
+            -1.0
+        } else {
+            (-b - discrim.sqrt()) / (2.0 * a)
+        }
+    }
+}
