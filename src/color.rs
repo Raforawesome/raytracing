903dc6c4f4536b3e6e4899f5e62907f67a4e8f55
Raforawesome <ubuntuhasnoroblox@gmail.com>
2025-08-29 13:00:13 -0400

    Implement multiple arithmetic traits for `Color`

--- src/color.rs
+++ src/color.rs
@@ -1,3 +1,8 @@
+use std::{
+    io::Write,
+    ops::{Add, Mul, Sub},
+};
+
 use crate::Vec3;
 
 #[derive(Copy, Clone, Debug)]
@@ -10,7 +15,39 @@ impl Color {
     }
 }
 
-pub fn write_color(pixel_color: Color) {
+impl Mul<f64> for Color {
+    type Output = Self;
+
+    fn mul(self, rhs: f64) -> Self::Output {
+        Color(Vec3(self.0.0 * rhs, self.0.1 * rhs, self.0.2 * rhs))
+    }
+}
+
+impl Add for Color {
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        Color(Vec3(
+            self.0.0 + rhs.0.0,
+            self.0.1 + rhs.0.1,
+            self.0.2 + rhs.0.2,
+        ))
+    }
+}
+
+impl Sub for Color {
+    type Output = Self;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        Color(Vec3(
+            self.0.0 - rhs.0.0,
+            self.0.1 - rhs.0.1,
+            self.0.2 - rhs.0.2,
+        ))
+    }
+}
+
+pub fn write_color(mut out: impl Write, pixel_color: Color) {
     let Color(Vec3(r, g, b)) = pixel_color;
 
     let r_byte: i32 = (255.999_f64 * r) as i32;
